26f6579286e73a5eb7af098fb3ed72f282132a0a
Dariusz Kowalec <dariusz.kowalec@billennium.com>
2020-12-15 21:47:12 +0100

    ccs style correction

--- index.js
+++ index.js
@@ -4,32 +4,37 @@ const aside = document.querySelector('.aside');
 const main_content = document.querySelector('.main')
 
 const showUserInformation = () => {
-    // aside.style.position = 'unset';
-    aside.style.display = 'flex';
-    aside.style.width = '100vw';
-    aside.style.height = '100%';
-    aside.style.zIndex = '1';
-    main_content.style.display = 'none'
+    console.log('aside',aside.style)
+    if (aside.style.display === '' || aside.style.display === 'none' ){
+        aside.style.display = 'flex';
+        aside.style.width = '100vw';
+        aside.style.height = '100%';
+        // aside.style.zIndex = '1';
+        main_content.style.display = 'none'
+    }
+
 }
 const closeUserInformation = () => {
-    aside.style.display = 'none';
-    main_content.style.display = 'flex'
-}
-
-const setAsideStyleToDesktop = () => {
-    if(window.innerWidth > 978) {
-        aside.style.display = 'flex';
-        aside.style.width = '20%';
-        aside.style.zIndex = '0';
-    } else {
+    if (aside.style.display === 'flex'){
         aside.style.display = 'none';
-        aside.style.width = '100vw';
-        aside.style.zIndex = '1';
+        main_content.style.display = 'flex'
     }
 }
+
+// const setAsideStyleToDesktop = () => {
+//     if(window.innerWidth > 1024) {
+//         aside.style.display = 'flex';
+//         aside.style.width = '20%';
+//         aside.style.zIndex = '0';
+//     } else {
+//         aside.style.display = 'none';
+//         aside.style.width = '100vw';
+//         aside.style.zIndex = '1';
+//     }
+// }
 mobileButton.addEventListener('click', showUserInformation)
 closeAside.addEventListener('click', closeUserInformation)
-window.addEventListener('resize', setAsideStyleToDesktop)
+// window.addEventListener('resize', setAsideStyleToDesktop)
 // window.onload = () => {
 //     document.querySelector('#downloadCv').addEventListener('click', ()=> {
 //         console.log('click')
