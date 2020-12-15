338f928ed80b9dea1a4deced47f221fbf5d79bb6
Dariusz Kowalec <dariusz.kowalec@billennium.com>
2020-12-15 22:16:43 +0100

    fix typo

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const aside = document.querySelector('.aside');
 const main_content = document.querySelector('.main')
 
 const showUserInformation = () => {
-    console.log('aside',aside.style)
     if (aside.style.display === '' || aside.style.display === 'none' ){
         aside.style.display = 'flex';
         aside.style.width = '100vw';
         aside.style.height = '100%';
-        // aside.style.zIndex = '1';
         main_content.style.display = 'none'
     }
-
 }
 const closeUserInformation = () => {
     if (aside.style.display === 'flex'){
@@ -20,29 +17,8 @@ const closeUserInformation = () => {
         main_content.style.display = 'flex'
     }
 }
-
-// const setAsideStyleToDesktop = () => {
-//     if(window.innerWidth > 1024) {
-//         aside.style.display = 'flex';
-//         aside.style.width = '20%';
-//         aside.style.zIndex = '0';
-//     } else {
-//         aside.style.display = 'none';
-//         aside.style.width = '100vw';
-//         aside.style.zIndex = '1';
-//     }
-// }
 mobileButton.addEventListener('click', showUserInformation)
 closeAside.addEventListener('click', closeUserInformation)
-// window.addEventListener('resize', setAsideStyleToDesktop)
-// window.onload = () => {
-//     document.querySelector('#downloadCv').addEventListener('click', ()=> {
-//         console.log('click')
-//         console.log(window)
-//         const cvData = document.querySelector('#cv');
-//         console.log(cvData)
-//         html2pdf().from(cvData).save();
-//     })
-// }
+
 
 
